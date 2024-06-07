803cddd86ee3f33bf4d38cdb49c59ccc85386f94
David Lin <>
2024-06-07 21:33:51 +1000

    add script to allow reseting example survey on close button click

--- docs/javascripts/doc.js
+++ docs/javascripts/doc.js
@@ -1,5 +1,5 @@
 window.addEventListener("surveychange", (e) => {
-  if (e.detail.name === "COMPLETE_OK") {
+  if (e.detail.name === "COMPLETE_OK" || e.detail.name === "SURVEY_CLOSE") {
     e.target.api.reset();
   }
 });
